a9f477d91d016799a63976b1d573be355f95ccb3
Guff05 <110795167+Guff05@users.noreply.github.com>
2024-05-01 02:55:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+
+grade = int(input("Enter your grade: "))
+
+if grade >= 90 and grade <= 100:
+  print("Good job you have an A")
+if grade >= 79 and grade <= 89:
+  print("you have a B not bad")
+if grade >= 55 and grade <= 68:
+  print("you have a D")
+if grade >= 0 and grade <= 54:
+  print("You are a falure")
