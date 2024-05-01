fcd526acec00ec09484a98a20acafb62bd8867dd
Guff05 <110795167+Guff05@users.noreply.github.com>
2024-04-30 22:09:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
+print("Exam Grade Calculator")
+print()
+name_of_exam = input("Name of exam: ")
+print()
+total_score = int(input("Max. Possible Score:"))
+your_score = int(input("Your score: "))
+print()
 
-grade = int(input("Enter your grade: "))
+number_score = float(your_score / total_score)
+final_number = round(number_score, 2)
+final_perc = round(float(your_score / total_score)*100, 2)
 
-if grade >= 90 and grade <= 100:
-  print("Good job you have an A")
-if grade >= 79 and grade <= 89:
-  print("you have a B not bad")
-if grade >= 55 and grade <= 68:
-  print("you have a D")
-if grade >= 0 and grade <= 54:
-  print("You are a falure")
+if final_number >= .90:
+  print("Your letter score is an A+")
+elif final_number >= .80 and final_number <= .89:
+  print("Your letter grade is an A-.")
+elif final_number >= .70 and final_number <= .79:
+  print("Your letter score is a B.")
+elif final_number >= .60 and final_number <= .69:
+  print("Your letter grade is a C.")
+elif final_number >= .50 and final_number <= .59:
+  print("Your letter grade is a D.")
+elif final_number <= .49:
+  print("Your letter grade is a U.")
+else: 
+  print("Try again!")
